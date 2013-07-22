625e9f1f99f6cbeb8bdb541beb41bcbea9597258
Marcos Piccinini <x@nofxx.com>
2013-07-22 08:51:13 -0300

    lil spec fix

--- spec/mongoid_geospatial/extensions/core_ext_spec.rb
+++ spec/mongoid_geospatial/extensions/core_ext_spec.rb
@@ -8,6 +8,10 @@ describe "Core Extensions" do
       [1,2,3].to_xy.should eql([1.0, 2.0])
     end
 
+  end
+
+  describe Hash do
+
     it "converts hash with symbol keys x,y to array with x,y" do
        { x: 1.1, y: 2.1 }.to_xy.should eql([1.1, 2.1])
     end
